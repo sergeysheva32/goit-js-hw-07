ba687796a45796643f160897c4bd5433eb115d55
sergeysheva32 <sergeysheva32@gmail.com>
2023-09-07 13:28:38 +0200

    homework07

---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.gallery');
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    (acc += `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`),
+  ''
+);
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
